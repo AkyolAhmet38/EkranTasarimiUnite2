18e0e1acee80dc014b7bff0df75e67ce37762d35
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-11-02 15:05:38 +0300

    no message

---
+++ app/src/main/java/com/example/mobiluygulamalar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobiluygulamalar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
